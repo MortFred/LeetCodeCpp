7b555788187b3eeabb9c5b7aee4a88046682f029
Morten Fredriksen <morfre@equinor.com>
2023-09-21 16:19:14 +0000

    Finish 334

--- 334_increasing_triplet_subsequence.cpp
+++ 334_increasing_triplet_subsequence.cpp
@@ -9,14 +9,27 @@ class Solution
 public:
     bool increasingTriplet(std::vector<int> &nums)
     {
-        return {};
+        if (nums.size() < 3)
+            return false;
+        int mid = INT32_MAX, left = INT32_MAX;
+
+        for (size_t i = 0; i != nums.size(); ++i)
+        {
+            if (nums[i] < left)
+                left = nums[i];
+            else if (left < nums[i] & nums[i] < mid)
+                mid = nums[i];
+            if (nums[i] > mid)
+                return true;
+        }
+        return false;
     }
 };
 
 int main()
 {
     Solution s;
-    std::vector<int> nums = {2, 3, 5, 1, 3};
+    std::vector<int> nums = {1, 1, -2, 6};
     bool res = s.increasingTriplet(nums);
     std::cout << res << std::endl;
     return 0;
